01611358d60ab97d8e7b99a2d9d4f0ae96eb4906
pattersonrptr <pattersonjunior@gmail.com>
2025-03-04 19:41:29 -0300

    Configure celery beat to call tasks in loop

--- scrapers/tasks.py
+++ scrapers/tasks.py
@@ -1,4 +1,5 @@
-from celery import Celery
+from celery import Celery, group
+from celery.schedules import crontab
 from scrapers.olx.scraper import Scraper
 import os
 import time
@@ -18,29 +19,19 @@ def run_scraper(search):
     time.sleep(sleep_time)
     print(f"RUNNING SCRAPER WITH {search} (slept for {sleep_time:.2f}s)")
     Scraper().run()
-
     return f"Done with {search} (slept for {sleep_time:.2f}s)"
 
-#TODO:
-# Criar uma task que chama run_scraper em loop, passando todos os search_tearms
-# Não vou agendar uma task para cada team, mas posso agendar uma que dispara várias
-
-# TODO:
-#  precisa ter o CHROME ou o FIREFOX e o CHROMEDRIVER ou o GECKODRIVER instalados para rodar o scraper
-
-
+@app.task(name="scrapers.tasks.run_scrapers")
+def run_scrapers():
+    searches = ["livro python", "livro java", "livro javascript"]
+    # Cria um grupo de tarefas para processamento paralelo
+    return group(run_scraper.s(search) for search in searches)()
 
 app.conf.beat_schedule = {
-    'run_scraper_every_60_seconds': {
-        'task': 'scrapers.tasks.run_scraper',
-        'schedule': 60.0,
-        'args': ("java",),
+    'run_scrapers_daily': {
+        'task': 'scrapers.tasks.run_scrapers',
+        'schedule': crontab(hour=0, minute=0),  # Meia-noite UTC
     },
-    # 'run_scraper_midnight': {
-    #     'task': 'scrapers.tasks.run_scraper',
-    #     'schedule': crontab(hour=0, minute=0),
-    #     'args': ("python",),
-    # }
 }
 
 app.conf.timezone = 'UTC'
