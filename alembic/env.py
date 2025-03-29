b9bae1c5f4ef1d2e8d7505467eba496395403c16
pattersonrptr <pattersonjunior@gmail.com>
2025-03-29 20:05:58 -0300

    Set python path in web container, stop inserting the path via env.py

--- alembic/env.py
+++ alembic/env.py
@@ -1,6 +1,4 @@
 # import logging
-import os
-import sys
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
@@ -9,7 +7,7 @@ from alembic import context
 from app.database import Base
 
 # Add projects's root directory
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
